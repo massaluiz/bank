1b17f6205e8c9ed7cabd6d64c3bfa40c014aac68
Luiz Massa <luimassapontes@gmail.com>
2020-05-23 20:15:25 -0300

    Adicionando Jquery na aplicação.

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,16 +1,16 @@
 abstract class View<T> {
 
-    protected _element: Element;
+    protected _element: JQuery;
 
     constructor(selector: string) {
 
-        this._element = document.querySelector(selector);
+        this._element = $(selector);
 
     }
 
     public update(model: T):void {
 
-        this._element.innerHTML = this.template(model);
+        this._element.html(this.template(model));
 
     }
 
